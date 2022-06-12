d025aa64311a0fd6b5b1ddd7c4ee446695e9428b
hartaithan <hartaithan@gmail.com>
2022-06-12 18:16:27 +0900

    basic scene with controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { PointerLockControls } from "@react-three/drei";
 
 const App = () => {
-  return <div className="App">Hello world!</div>;
+  return (
+    <Canvas dpr={window.devicePixelRatio}>
+      <ambientLight />
+      <mesh scale={1} position={[0, 0, 0]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshBasicMaterial color="white" wireframe />
+      </mesh>
+      {/* @ts-expect-error */}
+      <PointerLockControls />
+    </Canvas>
+  );
 };
 
 export default App;
