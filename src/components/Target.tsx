51066f11c4084d0973557f33f8534d04d2bd97d0
hartaithan <hartaithan@gmail.com>
2022-07-23 14:04:58 +0900

    handle hit on target and change position

--- src/components/Target.tsx
+++ src/components/Target.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 
+const randomNum = (min: number, max: number) =>
+  Math.floor(Math.random() * (max - min)) + min;
+
 const Target: React.FC = () => {
+  const [position, setPosition] = React.useState([0, 0, 0]);
+
+  const getNewPosition = () => {
+    const x = randomNum(-3, 3);
+    const y = randomNum(-3, 3);
+    setPosition([x, y, 0]);
+  };
+
+  const handleHit = () => {
+    getNewPosition();
+  };
+
   return (
     <mesh
       scale={0.5}
-      position={[0, 0, 0]}
+      position={position}
       rotation={[Math.PI / 2, 0, 0]}
-      onClick={() => console.info("hit")}
+      onClick={() => handleHit()}
     >
       <cylinderGeometry args={[1, 1, 1, 16]} />
       <meshBasicMaterial color="white" wireframe />
